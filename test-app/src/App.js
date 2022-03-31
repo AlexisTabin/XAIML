3ca27aaa6de3676e48a58e95c3082cfe1fc11383
alexis <alexis.tabin@gmail.com>
2022-03-31 10:52:04 +0200

    Implement a basic smiley emoji

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {arc} from 'd3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          j'Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const width = window.innerWidth;
+const height = window.innerHeight;
+const centerX = width / 2;
+const centerY = height / 2;
+const strokeWidth = 20;
+const eyeOffsetX = 90;
+const eyeOffsetY = 100;
+const eyeRadius = 40;
+const mouthWidth = 20;
+const mouthRadius = 300;
+
+const mouthArc = arc()
+  .innerRadius(mouthRadius)
+  .outerRadius(mouthRadius + mouthWidth)
+  .startAngle(Math.PI/2)
+  .endAngle(Math.PI*3/2)
+
+const App = () => (
+<svg width={width} height={height}>
+    <g transform={`translate(${centerX},${centerY})`}>
+      <circle
+        r={centerY - strokeWidth / 2}
+        fill="yellow"
+        stroke="black"
+        stroke-width={strokeWidth}
+      />
+      <circle
+        cx={-eyeOffsetX}
+        cy={-eyeOffsetY}
+        r={eyeRadius}
+      />
+      <circle
+        cx={eyeOffsetX}
+        cy={-eyeOffsetY}
+        r={eyeRadius}
+      />
+      <path 
+        d={mouthArc()}
+      />
+    </g>
+  </svg>
+);
 
 export default App;
